7d56ac6f5643559e1d496ee959f55f9477f225eb
ulugbek <udavlatboyev@gmail.com>
2025-12-10 13:03:48 +0500

    started project

---
+++ erp/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from erp.models import Actor, Movie
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = '__all__'
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+        read_only_fields = ['slug']
